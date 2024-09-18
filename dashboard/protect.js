eeaa405e4587457ea17471a46739d2519b2ee8f5
kemms-production <kemms_yt@outlook.com>
2024-09-18 16:36:58 +0200

    Update protect.js

--- dashboard/protect.js
+++ dashboard/protect.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Vérifier si l'utilisateur est connecté
     if (!localStorage.getItem('loggedIn')) {
         // Rediriger vers une page spécifique si non connecté
-        window.location.href = 'page-de-redirection.html'; // Remplacez par l'URL de la page souhaitée
+        window.location.href = 'https://lmn-montage.github.io/'; // Remplacez par l'URL de la page de connexion
     }
 });
+
